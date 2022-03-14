6aeb0e6adefe7e95a94230b95b8aba46ed705b95
husongping <2973721144@qq.com>
2022-03-14 21:24:16 +0800

    1

--- requeststest/requests01.py
+++ requeststest/requests01.py
@@ -1,11 +1,25 @@
 import requests                                         #导入requests
+from utils.db_tools import query                                   
 url = "http://www.weather.com.cn/data/sk/101010100.html"#定义变量url存放网站
 res = requests.get(url)                                 #试用get方法请求网址，把获取的结果保存在一个变量res中
 a = res.text                                            #获取返回值文本
 # print(a)
 
-
 #取返回值里面time的值
 b = res.json() #把返回值转换成字典
 print(b)
 print(b["weatherinfo"]["time"])#取出time值
+
+url1 = "http://132.232.44.158:8080/morning/getAllGoods"
+print(requests.get(url1).text)
+print(requests.get(url1).json)
+
+res1 =requests.get(url1)
+assert res1.status_code == 200
+assert res1.json()["success"] == True
+# 连接查询数据库
+aa = query("select * from tb_goods")
+assert len(aa)!=0
+print(aa)
+
+print("true")
--- requeststest/requests03.py
+++ requeststest/requests03.py
@@ -3,7 +3,7 @@ import requests
 
 #json：字典格式{"username":"abc","passwd","123456"}
 url = "http://132.232.44.158:5000/userLogin/"#接口地址
-data = {"username":"test", "password":"test", "captcha":"123456"}
+data = {"username":"test", "password":"123456", "captcha":"123456"}
 
 res = requests.post(url = url,json=data)#向url接口使用post方法传递data的json数据
 print(res.text)#打印返回值
